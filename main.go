df273c33b63903ad884f2d175a6ad791e8955c94
stackoverflowed9 <f20241294@pilani.bits-pilani.ac.in>
2025-02-26 23:44:26 +0530

    Added implementation of data validation

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	//"fmt"
 	"fmt"
 	"log"
-	"strings"
-
-	//"os"
-
+	"os"
+	"strconv"
+	"math"
 	"github.com/xuri/excelize/v2"
 )
 
 
+type Student struct {
+	ClassNo int
+	EmplID string
+    CampusID string
+    Quiz float64
+    MidSem float64
+    LabTest float64
+    WeeklyLabs float64
+    PreCompre float64
+    Compre float64
+    Total float64
+    ComputedSum float64
+}
+
+const tolerance = 0.0001
+
 func main(){
-	//file_path := os.Args[1]
+	file_path := os.Args[1]
 	
-	file, err := excelize.OpenFile("./CSF111_202425_01_GradeBook_stripped.xlsx")
+	file, err := excelize.OpenFile(file_path)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,22 +45,52 @@ func main(){
 	}
 	defer rows.Close()
 
-	//Checking whether any of the cell in any row is
+	var students []Student
 	var row_id = 1
 	rows.Next()
 	for rows.Next() {
-		row, _ := rows.Columns()
-		var flag = false
 		
-		for _, cell := range row {
-			if strings.TrimSpace(cell) == "" {
-				flag = true
-				break
-			}
+		row, _ := rows.Columns()
+		if len(row) == 0 {
+			continue
 		}
-		if !flag && len(row) != 0{
-			fmt.Printf("%d: %v\n", row_id, row)
-			row_id++
+		
+		classno, _ := strconv.Atoi(row[1])		
+		quiz, _ := strconv.ParseFloat(row[4], 32)
+		midSem, _ := strconv.ParseFloat(row[5], 32)
+		labTest, _ := strconv.ParseFloat(row[6], 32)
+		weeklyLabs, _ := strconv.ParseFloat(row[7], 32)
+		preCompre, _ := strconv.ParseFloat(row[8], 32)
+		compre, _ := strconv.ParseFloat(row[9], 32)
+		total, _ := strconv.ParseFloat(row[10], 32)
+		
+		
+
+		var computedSum float64 = quiz + midSem + labTest + weeklyLabs + compre
+		fmt.Printf("%f\n", computedSum)
+		student := Student{
+			ClassNo: classno,
+			EmplID: row[1],
+			Quiz: quiz,
+			MidSem: midSem,
+			LabTest: labTest,
+			WeeklyLabs: weeklyLabs,
+			PreCompre: preCompre,
+			Compre: compre,
+			Total: total,
+			ComputedSum: computedSum,
 		}
+
+		if math.Abs(computedSum-total) > tolerance {
+			log.Printf("!!! Error in row %d: Computed sum %.2f does not match given total %.2f\n", row_id, computedSum, total)
+		}
+	
+		students = append(students, student)
+		row_id++
+
+	}
+
+	for _, s := range students{
+		fmt.Printf("%+v\n", s)
 	}
 }
